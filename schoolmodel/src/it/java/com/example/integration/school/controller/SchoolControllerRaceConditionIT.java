8e08c87501478fd57044719d4dc6e2dbc28aff2c
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-04-10 22:22:09 +0200

    Test running with bare threads.
    This is the version of the book, which don't catch exceptions from the
    database.

--- schoolmodel/src/it/java/com/example/integration/school/controller/SchoolControllerRaceConditionIT.java
+++ schoolmodel/src/it/java/com/example/integration/school/controller/SchoolControllerRaceConditionIT.java
@@ -15,9 +15,14 @@ import com.example.integration.school.model.Student;
 import com.example.integration.school.repository.*;
 import com.example.integration.school.view.StudentView;
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
 
 import org.assertj.swing.timing.*;
+import org.bson.Document;
+
 import static org.awaitility.Awaitility.*;
 
 import org.junit.Before;
@@ -39,6 +44,9 @@ public class SchoolControllerRaceConditionIT {
 		MongoClient client = new MongoClient("localhost");
 		MongoDatabase database = client.getDatabase(StudentMongoRepository.SCHOOL_DB_NAME);
 		database.drop();
+		MongoCollection<Document> studenCollection = database
+				.getCollection(StudentMongoRepository.STUDENT_COLLECTION_NAME);
+		studenCollection.createIndex(Indexes.ascending("id"), new IndexOptions().unique(true));
 		studentRepository = new StudentMongoRepository(client);
 	}
 
@@ -46,32 +54,13 @@ public class SchoolControllerRaceConditionIT {
 	public void test_newStudent_Concurrent() {
 		Student student = new Student("1", "Mario");
 		ExecutorService executor = Executors.newFixedThreadPool(THREADS_NUM);
-		
-		
-		// imperative language is definitely more readable to me
-		List<Future<?>> futures = new ArrayList<>(THREADS_NUM);
-		for (int i = 0; i < THREADS_NUM; i++) {
-			futures.add(executor.submit(() -> new SchoolController(studentRepository, studentView).newStudent(student)));
-		}
 
-		// functional version, too cluttered in this case. Slightly slower.
-//		List<Future<?>> futures = IntStream.range(0, THREADS_NUM).
-//				mapToObj(i -> executor.submit(() -> new SchoolController(studentRepository, studentView).newStudent(student))).
-//				collect(Collectors.toList());
-		
-		// Standard assertj swing, no need of Awaitility.
-		Pause.pause(new Condition("Wait for threads to terminate") {
-			
-			@Override
-			public boolean test() {
-				return futures.stream().allMatch(p -> p.isDone());
-			}
-			
-		},Timeout.timeout(5, TimeUnit.SECONDS));
+		List<Thread> threads = IntStream.range(0, THREADS_NUM)
+				.mapToObj(
+						i -> new Thread(() -> new SchoolController(studentRepository, studentView).newStudent(student)))
+				.peek(t -> t.start()).collect(Collectors.toList());
 
-		//Awaitility, don't require anonymous class. Definetly slower.
-//		await().atMost(5, TimeUnit.SECONDS).until(() -> futures.stream().allMatch(p -> p.isDone()));
-		
+		await().atMost(5, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
 		assertThat(studentRepository.findAll()).containsExactly(student);
 		executor.shutdown();
 	}
