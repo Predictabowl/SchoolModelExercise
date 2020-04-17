8cea1d394228be8749261d8047cbe0a17b3bb8b3
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-04-17 20:03:35 +0200

    Added E2E tests.
    Update the pom for the e2e folder.

--- schoolmodel/src/main/java/com/example/integration/school/app/swing/SchoolSwingApp.java
+++ schoolmodel/src/main/java/com/example/integration/school/app/swing/SchoolSwingApp.java
@@ -1,6 +1,7 @@
 package com.example.integration.school.app.swing;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 
 import com.example.integration.school.controller.SchoolController;
 import com.example.integration.school.repository.StudentMongoRepository;
@@ -8,23 +9,39 @@ import com.example.integration.school.view.StudentSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class SchoolSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(mixinStandardHelpOptions = true)
+public class SchoolSwingApp implements Callable<Void>{
+	
+	@Option(names = {"--mongo-host"}, description = {"MongoDB host address"})
+	private String mongoHost = "localhost";
+	
+	@Option(names = {"--mongo-port"}, description = {"MongoDB host port"})
+	private int mongoPort = 27017;
+	
+	@Option(names = {"--db-name"}, description = {"Database name"})
+	private String databaseName ="school";
+	
+	@Option(names = {"--db-collection"}, description = {"Collection name"})
+	private String collectionName = "student";
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		new CommandLine(new SchoolSwingApp()).execute(args);
+	}
+	
+	@Override
+	public Void call() throws Exception{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					String mongoHost = "localhost";
-					int mongoPort = 27017;
-					if (args.length > 0)
-						mongoHost = args[0];
-					if (args.length > 1)
-						mongoPort = Integer.parseInt(args[1]);
 					StudentMongoRepository studentRepository = new StudentMongoRepository(
-							new MongoClient(new ServerAddress(mongoHost, mongoPort)), "school", "student");
+							new MongoClient(new ServerAddress(mongoHost, mongoPort)), databaseName, collectionName);
 					StudentSwingView studentView = new StudentSwingView();
 					SchoolController schoolController = new SchoolController(studentRepository, studentView);
 					studentView.setSchoolController(schoolController);
@@ -36,6 +53,7 @@ public class SchoolSwingApp {
 				}
 			}
 		});
+		return null;
 	}
 
 }
