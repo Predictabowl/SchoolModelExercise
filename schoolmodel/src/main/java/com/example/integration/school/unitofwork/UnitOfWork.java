cc99fe8dc7af7b6f5fddaa44dd3abbb299fa6ea0
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-04-28 23:14:55 +0200

    Removed UnitOfWork

--- schoolmodel/src/main/java/com/example/integration/school/unitofwork/UnitOfWork.java
+++
@@ -1,8 +0,0 @@
-package com.example.integration.school.unitofwork;
-
-import com.example.integration.school.repository.StudentRepository;
-
-public interface UnitOfWork {
-	public StudentRepository getRepository();
-	public void commit();
-}
